62272888b5bcfcfd6795fc421322cecf3a934a4e
Danny Wahl <danny@iywahl.com>
2024-04-30 08:36:55 -0600

    rewrite billboard to use coloroverrides

--- components/AppLayout/themeOverrides/billboard.ts
+++ components/AppLayout/themeOverrides/billboard.ts
@@ -1,69 +1,18 @@
-import {
-  canvasHighContrast as highContrast,
-  canvas as standard,
-} from "@instructure/ui";
-import * as colorOverrides from "./colors";
+import type { Mode, UI } from "types";
+import { colors } from "../themeOverrides";
 
-const billboardOverrides = (isDark: boolean, isHighContrast: boolean) => {
-  const standardThemeOverride = {
-    iconColor: isDark ? standard.colors.textLightest : standard.colors.textDark,
-    iconHoverColor: isDark
-      ? colorOverrides.darkOxfordBrand
-      : standard.colors.textInfo,
-    backgroundColor: isDark
-      ? colorOverrides.darkStandardOxford
-      : standard.colors.backgroundLight,
-    iconHoverColorInverse: isDark
-      ? standard.colors.textDarkest
-      : standard.colors.textLight,
-    messageColor: isDark
-      ? standard.colors.textLightest
-      : standard.colors.textDark,
-    messageColorClickable: isDark
-      ? colorOverrides.darkOxfordBrand
-      : standard.colors.textInfo,
-    messageColorInverse: isDark
-      ? standard.colors.textDarkest
-      : standard.colors.textLight,
-    clickableActiveBg: isDark
-      ? colorOverrides.darkOxfordBrand
-      : standard.colors.backgroundInfo,
-    clickableActiveText: isDark
-      ? standard.colors.textDarkest
-      : standard.colors.textLight,
+const billboardOverrides = (mode: Mode, ui: UI) => {
+  return {
+    iconColor: colors[ui][mode].text.dark,
+    iconHoverColor: colors[ui][mode].text.info,
+    backgroundColor: colors[ui][mode].background.lightest,
+    iconHoverColorInverse: colors[ui][mode].text.lightest,
+    messageColor: colors[ui][mode].text.dark,
+    messageColorClickable: colors[ui][mode].text.info,
+    messageColorInverse: colors[ui][mode].text.light,
+    clickableActiveBg: colors[ui][mode].background.info,
+    clickableActiveText: colors[ui][mode].text.lightest,
   };
-
-  const highContrastThemeOverride = {
-    iconColor: isDark
-      ? highContrast.colors.textLightest
-      : highContrast.colors.textDark,
-    iconHoverColor: isDark
-      ? colorOverrides.darkHighContrastBrand
-      : standard.colors.textInfo,
-    backgroundColor: isDark
-      ? colorOverrides.black
-      : highContrast.colors.backgroundLight,
-    iconHoverColorInverse: isDark
-      ? colorOverrides.black
-      : highContrast.colors.textLight,
-    messageColor: isDark
-      ? highContrast.colors.textLightest
-      : highContrast.colors.textDark,
-    messageColorClickable: isDark
-      ? colorOverrides.darkHighContrastBrand
-      : highContrast.colors.textInfo,
-    messageColorInverse: isDark
-      ? colorOverrides.black
-      : highContrast.colors.textLight,
-    clickableActiveBg: isDark
-      ? colorOverrides.darkHighContrastBrand
-      : highContrast.colors.backgroundInfo,
-    clickableActiveText: isDark
-      ? colorOverrides.black
-      : highContrast.colors.textLight,
-  };
-
-  return isHighContrast ? highContrastThemeOverride : standardThemeOverride;
 };
 
 export default billboardOverrides;
