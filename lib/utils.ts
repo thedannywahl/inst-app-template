6e73f11e8291f29840ef67418623757aa0156334
Danny Wahl <danny@iywahl.com>
2024-05-14 11:24:47 -0600

    handle 3 character hex codes

--- lib/utils.ts
+++ lib/utils.ts
@@ -26,7 +26,8 @@ const signature = (fn: (...args: unknown[]) => void): string => {
 /**
  * Lightens or darkens a color with a specified percentage.
  *
- * @param {RGB} color - The color to mix. This should be a string in the format "rgb(r g b)" or "rgb(r g b / a)".
+ * @param {RGB} color - The color to mix. This should be a string in the format
+ * "rgb[a](r g b[ / a])", "#rgb", or "#rrggbb[aa]".
  * @param {number} amount - The percentage to mix the color by. This should be a number between -100 and 100.
  * If the amount is less than -100, it will be treated as -100. If the amount is more than 100, it will be treated as 100.
  *
@@ -34,26 +35,32 @@ const signature = (fn: (...args: unknown[]) => void): string => {
  * where r, g, and b are the mixed red, green, and blue color components, and a is the original alpha component or 1.
  *
  * @throws {Error} Will throw an error if the color format is invalid.
- * 
+ *
  * @example mix("rgb(255 0 0)", 50); // "rgb(255 128 128 / 1)"
  */
 const mix = (color: RGB, amount: number): string => {
   const err = `Invalid color format ${color}`;
+  let c = color;
   let rgb = [] as number[];
   let match = null as RegExpMatchArray | null;
-  if (color.charAt(0) === "#") {
-    const hexRegex = /^#?([a-f\d])([a-f\d])([a-f\d])([a-f\d])([a-f\d])([a-f\d])$/i;
-    match = color.match(hexRegex)
+  if (c.charAt(0) === "#") {
+    if (c.length === 4) c = `#${c.slice(1).replaceAll(/./g, "$&$&")}`;
+    const hexRegex = /^#(([a-f\d]{6}([a-f\d]{2})?))$/i;
+    match = c.match(hexRegex);
     if (!match) throw new Error(err);
-    rgb = match.slice(1).map(v => Number.parseInt(v + v, 16))
+    for (let i = 0; i < 6; i += 2) {
+      rgb.push(Number.parseInt(match[1][i] + match[1][i + 1], 16));
+    }
+    if (typeof match[3] !== "undefined") {
+      rgb.push(
+        Number.parseFloat((Number.parseInt(match[3], 16) / 255).toFixed(2)),
+      );
+    }
   } else {
-    const rgbRegEx = /\(([^)]+)\)/
-    const match = color.match(rgbRegEx);
+    const rgbRegEx = /\(([^)]+)\)/;
+    const match = c.match(rgbRegEx);
     if (!match) throw new Error(err);
-    rgb = match[1]
-      .split(" ")
-      .filter((v) => v !== "/")
-      .map(Number);
+    rgb = match[1].split(/[\s,\/]+/).map(Number);
   }
   const shaded = rgb
     .slice(0, 3)
