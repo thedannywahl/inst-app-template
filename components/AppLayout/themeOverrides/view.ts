fbf3eca56d188c3c05b727849642ebe72784ae67
Danny Wahl <danny@iywahl.com>
2024-05-14 18:30:59 -0600

    use color overrides for View

--- components/AppLayout/themeOverrides/view.ts
+++ components/AppLayout/themeOverrides/view.ts
@@ -4,8 +4,31 @@ import { colors } from "../themeOverrides";
 const viewOverrides = (mode: Mode, ui: UI) => {
   const color = colors[ui][mode];
   return {
-    backgroundPrimary: color.lightest,
     color: color.darkest,
+    colorPrimaryInverse: color.lightest,
+    borderColorPrimary: color.light,
+    borderColorSecondary: color.dark,
+    borderColorSuccess: color.success,
+    borderColorBrand: color.info,
+    borderColorInfo: color.info,
+    borderColorAlert: color.alert,
+    borderColorWarning: color.warning,
+    borderColorDanger: color.danger,
+    borderColorTransparent: colors.fixed.transparent,
+    debugOutlineColor: color.danger,
+    backgroundPrimary: color.lightest,
+    backgroundSecondary: color.lighter,
+    backgroundPrimaryInverse: color.darkest,
+    backgroundBrand: color.info,
+    backgroundInfo: color.info,
+    backgroundAlert: color.alert,
+    backgroundSuccess: color.success,
+    backgroundDanger: color.danger,
+    backgroundWarning: color.warning,
+    focusColorInfo: color.info,
+    focusColorDanger: color.danger,
+    focusColorSuccess: color.success,
+    focusColorInverse: color.lightest,
   };
 };
 
